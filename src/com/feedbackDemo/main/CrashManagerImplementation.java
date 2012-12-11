7d6e3ac412cec4e0896756b263d9700812f2a7e0
Bogdan Nistor <bogdan.nistorflorian@gmail.com>
2012-12-11 18:23:19 +0200

    Implemented CrashManagerImplementation based on new implementation of CrashManager

---
+++ src/com/feedbackDemo/main/CrashManagerImplementation.java
@@ -0,0 +1,99 @@
+package com.feedbackDemo.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+
+import android.app.Activity;
+import android.provider.Settings;
+import android.widget.Toast;
+import net.hockeyapp.android.CrashManagerListener;
+
+/**
+ * {@link CrashManagerListener} implementation
+ * @author Bogdan Nistor
+ *
+ */
+public class CrashManagerImplementation extends CrashManagerListener {
+  private Activity activity;
+  
+  /**
+   * Constructor
+   * @param weakActivity
+   */
+  public CrashManagerImplementation(WeakReference<Activity> weakActivity) {
+    if (weakActivity != null) {
+      activity = weakActivity.get();
+    }
+  }
+  
+  public Boolean ignoreDefaultHandler() {
+    return true;
+  }
+  
+  public String getContact() {
+    return "joe.user@gmail.com";
+  }
+  
+  public String getUserID() {
+    if (activity == null) {
+      return null;
+    }
+    
+    return Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
+  }
+  
+  public Boolean onCrashesFound() {
+    return true;
+  }
+  
+  public void onCrashesSent() {
+    if (activity != null) {
+      activity.runOnUiThread(new Runnable() {
+        
+        @Override
+        public void run() {
+          Toast.makeText(activity, "Crash data was sent. Thanks!", Toast.LENGTH_LONG).show();            
+        }
+      });
+    }
+  }
+  
+  public void onCrashesNotSent() {
+    if (activity != null) {
+      activity.runOnUiThread(new Runnable() {
+        
+        @Override
+        public void run() {
+          Toast.makeText(activity, "Crash data failed to sent. Please try again later.", Toast.LENGTH_LONG).show();
+        }
+      });
+    }
+  }
+  
+  public String getDescription() {
+    String description = "";
+
+    try {
+      Process process = Runtime.getRuntime().exec("logcat -d HockeyApp:D *:S");
+      BufferedReader bufferedReader = 
+        new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      StringBuilder log = new StringBuilder();
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        log.append(line);
+        log.append(System.getProperty("line.separator"));
+      }
+      bufferedReader.close();
+
+      description = log.toString();
+    } 
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return description;
+  }
+}
--- src/com/feedbackDemo/main/MainActivity.java
+++ src/com/feedbackDemo/main/MainActivity.java
@@ -1,67 +1,98 @@
 package com.feedbackDemo.main;
 
+import java.lang.ref.WeakReference;
+
 import utils.AppConstants;
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.FeedbackManager;
 import net.hockeyapp.android.UpdateManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+  private Context mContext;
+  private CrashManagerImplementation crashManagerImpl;
+  private CrashManager crashManager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mContext = this;
+    registerForCrashes();
+    checkForUpdates();
+    registerForFeedback();
 		
-		checkForUpdates();
-		registerForFeedback();
-	}
-	
-	private void checkForCrashes() {
-		CrashManager.register(this, AppConstants.APP_ID);
-	}
-	
-	private void registerForFeedback() {
-		FeedbackManager.register(this, AppConstants.APP_ID);
-	}
+    Button button = (Button) findViewById(R.id.do_some_wrong_stuff_button);
+    button.setOnClickListener(this);
+  }
 
-	private void checkForUpdates() {
-		/** Remove this for store builds! */
-		UpdateManager.register(this, AppConstants.APP_ID);
-	}
+  private void registerForCrashes() {
+    crashManagerImpl = new CrashManagerImplementation(new WeakReference<Activity>(MainActivity.this));
+    crashManager = new CrashManager(new WeakReference<Context>(mContext));
+    crashManager.register(AppConstants.APP_ID, crashManagerImpl);
+  }
 	
-	private void showFeedbackActivity() {
-		FeedbackManager.showFeedbackActivity(this);
-	}
+  private void registerForFeedback() {
+    FeedbackManager.register(this, AppConstants.APP_ID);
+  }
+
+  private void checkForUpdates() {
+    /** Remove this for store builds! */
+    UpdateManager.register(this, AppConstants.APP_ID);
+  }
 	
-	@Override
-	public void onResume() {
-		super.onResume();
-		checkForCrashes();
-	}
+  private void showFeedbackActivity() {
+    FeedbackManager.showFeedbackActivity(this);
+  }
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.activity_main_menu, menu);
+  @Override
+  public void onResume() {
+    super.onResume();
+  }
+	
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.activity_main_menu, menu);
 	    
-	    return true;
-	}
+    return true;
+  }
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    /** Handle item selection */
-	    switch (item.getItemId()) {
-	        case R.id.menu_feedback:
-	            showFeedbackActivity();
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    /** Handle item selection */
+    switch (item.getItemId()) {
+      case R.id.menu_feedback:
+        showFeedbackActivity();
 	            
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
+        return true;
+        
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.do_some_wrong_stuff_button:
+        TextView unexistentTextView = null;
+        unexistentTextView.setText("Dummy text");
+        
+        break;
+  
+      default:
+        break;
+    }
+  }
 }
